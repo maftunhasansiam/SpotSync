d273c2f00010380e158a0c93c34035f7c3ae723c
maftunhasansiam <maftunhasansiam@gmail.com>
2026-06-29 23:52:22 +0600

    feat: add parking zone repository with CRUD and active count

---
+++ repository/zone_repository.go
@@ -0,0 +1,59 @@
+package repository
+
+import (
+	"github.com/maftunhasansiam/SpotSync/models"
+	"gorm.io/gorm"
+)
+
+type ZoneRepository interface {
+	Create(zone *models.ParkingZone) error
+	FindAll() ([]models.ParkingZone, error)
+	FindByID(id uint) (*models.ParkingZone, error)
+	Update(zone *models.ParkingZone) error
+	Delete(id uint) error
+	CountActiveReservations(zoneID uint) (int64, error)
+}
+
+type zoneRepository struct {
+	db *gorm.DB
+}
+
+func NewZoneRepository(db *gorm.DB) ZoneRepository {
+	return &zoneRepository{db: db}
+}
+
+func (r *zoneRepository) Create(zone *models.ParkingZone) error {
+	return r.db.Create(zone).Error
+}
+
+func (r *zoneRepository) FindAll() ([]models.ParkingZone, error) {
+	var zones []models.ParkingZone
+	err := r.db.Find(&zones).Error
+	return zones, err
+}
+
+func (r *zoneRepository) FindByID(id uint) (*models.ParkingZone, error) {
+	var zone models.ParkingZone
+	err := r.db.First(&zone, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &zone, nil
+}
+
+func (r *zoneRepository) Update(zone *models.ParkingZone) error {
+	return r.db.Save(zone).Error
+}
+
+func (r *zoneRepository) Delete(id uint) error {
+	return r.db.Delete(&models.ParkingZone{}, id).Error
+}
+
+
+func (r *zoneRepository) CountActiveReservations(zoneID uint) (int64, error) {
+	var count int64
+	err := r.db.Model(&models.Reservation{}).
+		Where("zone_id = ? AND status = ?", zoneID, "active").
+		Count(&count).Error
+	return count, err
+}
